4ef48a75ae7d8a441f64db25be9d7e8c24c8dd3b
ChrinovicM <chrinovicmukulu93@gmail.com>
2022-12-11 04:05:53 -0500

    first commit

---
+++ Document_files/index.js
@@ -0,0 +1,10 @@
+const hourE1 = document.querySelector("hour")
+const minuteE1 = document.querySelector("minute")
+const secondE1 = document.querySelector("second")
+
+function updateClock() {
+    const currentDate = new Date();
+    console.log(currentDate);
+}
+
+updateClock();
